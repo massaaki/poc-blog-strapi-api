ce10d4e4c8f3cb1f0dfc0748585f042958f617a7
Massaaki <mauricio.massaaki@gmail.com>
2020-11-20 23:49:15 -0300

    started Strapi with Postgres sql

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Start database container
+2. Start strapi container
+3. Access http://localhost:1337/
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '79835d87-905a-4e96-9478-29bc4f21e94e'
+};
